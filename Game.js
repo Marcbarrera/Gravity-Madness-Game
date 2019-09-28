aa7ff2bfc04eeeacfe740a8b6b728ce72079ace7
marcbarrera <barreramarinmarc@gmail.com>
2019-09-28 21:41:16 +0200

    code refactorized

--- Game.js
+++ Game.js
@@ -14,117 +14,111 @@ function Game (canvas){
 
 Game.prototype.startGame = function () {
 
-    this.player = new Player(this.canvas);
-    var loop = () => {
-        if(Math.random() > 0.98) {
-            var randomX = Math.random() * this.canvas.width - 10;
-            var newEnemy = new Enemy(this.canvas, randomX);
-
-            this.enemies.push(newEnemy);
-          }
-          if(Math.random() > 0.98) {
-            var randomX = Math.random() * this.canvas.width - 10;
-            var newCross = new Cross(this.canvas, randomX);
-
-            this.crosses.push(newCross);
-          }
-          this.update();
-          this.clear();
-          this.draw();
-          this.checkEnemisInScreen();
-          this.checkCollisions();
-          if(!this.isGameOver) {
-            requestAnimationFrame(loop)
-          } 
-          
-        
-          else {
-            this.onGameOver();
-          }
-        };
-        loop();
+  this.player = new Player(this.canvas);
+  var loop = () => {
+    if(Math.random() > 0.98) {
+        var randomX = Math.random() * this.canvas.width - 10;
+        var newEnemy = new Enemy(this.canvas, randomX);
+        this.enemies.push(newEnemy);
+      }
+    if(Math.random() > 0.98) {
+        var randomX = Math.random() * this.canvas.width - 10;
+        var newCross = new Cross(this.canvas, randomX);
+        this.crosses.push(newCross);
+      }
+    this.update();
+    this.clear();
+    this.draw();
+    this.checkEnemisInScreen();
+    this.checkCollisions();
+    if(!this.isGameOver) {
+      requestAnimationFrame(loop)
+    } 
+    else {
+      this.onGameOver();
+    }
+    };
+    loop();
 }
 Game.prototype.update = function() {
-    this.player.move();
-    this.enemies.forEach(function(enemy) {
-      enemy.move();
-    })
-    this.crosses.forEach(function(cross) {
-        cross.move();
-      })
+  this.player.move();
+  this.enemies.forEach(function(enemy) {
+    enemy.move();
+  })
+  this.crosses.forEach(function(cross) {
+      cross.move();
+  })
 
-      var numeroApples = document.querySelector('.numApples p');
-      numeroApples.innerHTML=+this.player.caughtApples;
+  var numeroApples = document.querySelector('.numApples p');
+  numeroApples.innerHTML=+this.player.caughtApples;
 
-      var numeroDeVidas = document.querySelector('.numLives p');
-      numeroDeVidas.innerHTML=+this.player.lives;
+  var numeroDeVidas = document.querySelector('.numLives p');
+  numeroDeVidas.innerHTML=+this.player.lives;
   }
 
 Game.prototype.clear = function() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 }
 
 Game.prototype.draw = function() {
-    this.player.draw();
-    
-    this.enemies.forEach(function(enemy) {
-      enemy.draw();
+  this.player.draw();
+
+  this.enemies.forEach(function(enemy) {
+    enemy.draw();
+  })
+  this.crosses.forEach(function(cross) {
+      cross.draw();
     })
-    this.crosses.forEach(function(cross) {
-        cross.draw();
-      })
-  }
+}
 
 
 Game.prototype.checkCollisions = function() {
     
-    this.enemies.forEach((enemy, index) => {
-      var rightLeft = this.player.x + this.player.width >= enemy.x;
-      var leftRight = this.player.x <= enemy.x + enemy.width;
-      var bottomTop = this.player.y + this.player.height >= enemy.y;
-      var topBottom = this.player.y <= enemy.y + enemy.height;
-  
-      if (rightLeft && leftRight && bottomTop && topBottom) {
-        this.enemies.splice(index, 1);
-        this.player.lives --;
-        if(this.player.lives === 0) {
+  this.enemies.forEach((enemy, index) => {
+    var rightLeft = this.player.x + this.player.width >= enemy.x;
+    var leftRight = this.player.x <= enemy.x + enemy.width;
+    var bottomTop = this.player.y + this.player.height >= enemy.y;
+    var topBottom = this.player.y <= enemy.y + enemy.height;
+
+    if (rightLeft && leftRight && bottomTop && topBottom) {
+      this.enemies.splice(index, 1);
+      this.player.lives --;
+      if(this.player.lives === 0) {
+        this.isGameOver = true;
+      }
+    }
+  })
+  this.crosses.forEach((cross, index) => {
+    var rightLeft1 = this.player.x + this.player.width >= cross.x;
+    var leftRight1 = this.player.x <= cross.x + cross.width;
+    var bottomTop1 = this.player.y + this.player.height >= cross.y;
+    var topBottom1 = this.player.y <= cross.y + cross.height;
+
+    if (rightLeft1 && leftRight1 && bottomTop1 && topBottom1) {
+      this.crosses.splice(index, 1);
+      this.player.caughtApples ++;
+    if(this.player.caughtApples ===5) {
           this.isGameOver = true;
-        }
-      }
-    })
-    this.crosses.forEach((cross, index) => {
-        var rightLeft1 = this.player.x + this.player.width >= cross.x;
-        var leftRight1 = this.player.x <= cross.x + cross.width;
-        var bottomTop1 = this.player.y + this.player.height >= cross.y;
-        var topBottom1 = this.player.y <= cross.y + cross.height;
-    
-        if (rightLeft1 && leftRight1 && bottomTop1 && topBottom1) {
+    }
+  }
+  })
+}
+
+Game.prototype.gameOverCallback = function(callback) {
+  this.onGameOver = callback;
+}
+
+Game.prototype.checkEnemisInScreen = function() {
+
+  this.enemies.forEach((enemy, index) => {
+    if (enemy.y > this.canvas.height) {
+          this.enemies.splice(index, 1);
+    }
+  })
+
+  this.crosses.forEach((cross, index) => {
+      if (cross.y > this.canvas.height) {
           this.crosses.splice(index, 1);
-          this.player.caughtApples ++;
-        if(this.player.caughtApples ===5) {
-             this.isGameOver = true;
-      }
-        }
-      })
-
-    
-  }
-
-  Game.prototype.gameOverCallback = function(callback) {
-    this.onGameOver = callback;
-  }
-
-  Game.prototype.checkEnemisInScreen = function() {
-
-      this.enemies.forEach((enemy, index) => {
-        if (enemy.y > this.canvas.height) {
-            this.enemies.splice(index, 1);
-        }
-      })
-
-    this.crosses.forEach((cross, index) => {
-        if (cross.y > this.canvas.height) {
-            this.crosses.splice(index, 1);
-        }
-    })
+    }
+  })
 }
--- main.js
+++ main.js
@@ -1,124 +1,104 @@
 'use strict'
 
 function main (){ 
+	var mainElement = document.querySelector('#site-main');
+	function buildDom (html){
+		mainElement.innerHTML = html;
+		return mainElement;
+	};
 
-    var mainElement = document.querySelector('#site-main');
+	function createWelcomeScreen (){
+		var wellcomeScreen = buildDom(`
+			<section id="intro-page">
+				<h2>WELCOME TO...</h2>
+				<h1><span class="title-first">GRAVITY</span> MADNESS</h1>
+				<button>START</button>
+			</section>
+			`);
+		var startButton = document.querySelector('button');
+		startButton.addEventListener('click',createGameScreen);
+	};
 
-    function buildDom (html){
-        mainElement.innerHTML = html;
-        return mainElement;
+	function createGameScreen(){
+		var GameScreen = buildDom(`
+			<section id="canvas-container">
+				<article class="legend">
+					<div class="numApples">
+						<div class="numApples-img">
+							<img src="./apple-pixel.png">
+						</div>
+						<div class="equal-img">
+							<img src="./equal.png">
+						</div>
+						<p></p>
+					</div>
+					<div class="numLives">
+						<div class="numLives-img">
+							<img src="./newton-pixel.png">
+						</div>
+					<div class="equal-img">
+						<img src="./equal.png">
+					</div>
+					<p></p>
+					</div>
+				</article>
+					<canvas></canvas> 
+				<article class="floor">
+				</article>
+			</section>
+		`);
 
-    };
+		var container = document.querySelector('#canvas-container');
+		var legendElement = document.querySelector('.legend');
+		var canvasElement = document.querySelector('canvas');
+						
+			legendElement.width = container.offsetWidth;
+			legendElement.height = container.offsetHeight;
 
-    function createWelcomeScreen (){
-        var wellcomeScreen = buildDom(`
-        <section id="intro-page">
-            <h2>WELCOME TO...</h2>
-            <h1><span class="title-first">GRAVITY</span> MADNESS</h1>
-            <button>START</button>
-        </section>
-        `);
+			canvasElement.width = container.offsetWidth;
+			canvasElement.height = container.offsetHeight;
+			canvasElement.margin = container.offsetMargin;
 
-        var startButton = document.querySelector('button');
-        startButton.addEventListener('click',createGameScreen);
-    };
+		var gameInstance = new Game (canvasElement);
+			gameInstance.startGame(); 
+			document.addEventListener('keydown', function(event){
+				if(event.key === 'ArrowLeft') {
+					gameInstance.player.setDirection(-1);
+				} else if (event.key === 'ArrowRight') {
+					gameInstance.player.setDirection(+1);
+				}
+			});
 
+		gameInstance.gameOverCallback(createGameOverScreen);
+	};
+	
+	function createGameOverScreen (){
+		if (this.player.lives===0){
+			var gameOverScreen = buildDom(`
+			<section id="outro-page">
+			<h1><span class="title-three">GAME</span>OVER</h1>
+			<button>Play Again</button>
+			</section>
+			`);
+			
+			var restartButton = document.querySelector('button');
+			restartButton.addEventListener ('click',createGameScreen);
+		}
+		else {
+			var gameOverWinScreen = buildDom(`
+			<section id="outro-page">
+			<h1><span class="title-four">YOU</span>WIN</h1>
+			<button>Play Again</button>
+			</section>
+			`);
 
-    function createGameScreen(){
-        var GameScreen = buildDom(`
-        <section id="canvas-container">
-            <article class="legend">
-                <div class="numApples">
-                    <div class="numApples-img">
-                        <img src="./apple-pixel.png">
-                    </div>
-                    <div class="equal-img">
-                        <img src="./equal.png">
-                    </div>
-                    <p>
-                    </p>
-                </div>
-                <div class="numLives">
-                    <div class="numLives-img">
-                        <img src="./newton-pixel.png">
-                    </div>
-                    <div class="equal-img">
-                        <img src="./equal.png">
-                    </div>
-                    <p>
-                    </p>
-                </div>
-            </article>
-            <canvas></canvas> 
-            <article class="floor"></article>
-        </section>
-        `);
-
-        var container = document.querySelector('#canvas-container');
-        var legendElement = document.querySelector('.legend');
-        var canvasElement = document.querySelector('canvas');
-        
-
-        legendElement.width = container.offsetWidth;
-        legendElement.height = container.offsetHeight;
-
-        canvasElement.width = container.offsetWidth;
-        canvasElement.height = container.offsetHeight;
-        canvasElement.margin = container.offsetMargin;
-
-
-        var gameInstance = new Game (canvasElement);
-
-        gameInstance.startGame(); 
-        document.addEventListener('keydown', function(event) {
-            if(event.key === 'ArrowLeft') {
-              gameInstance.player.setDirection(-1);
-            } else if(event.key === 'ArrowRight') {
-              gameInstance.player.setDirection(+1);
-            }
-          });
-        gameInstance.gameOverCallback(createGameOverScreen);
-
-
-
-        //setTimeout(createGameOVerScreen, 2000);
-
-
-    };
-
-
-    function createGameOverScreen (){
-        if (this.player.lives===0){
-        var gameOverScreen = buildDom(`
-        <section id="outro-page">
-        <h1><span class="title-three">GAME</span>OVER</h1>
-        <button>Play Again</button>
-        </section>
-        `);
-
-        var restartButton = document.querySelector('button');
-        restartButton.addEventListener ('click',createGameScreen);
-        }
-        else {
-            var gameOverWinScreen = buildDom(`
-            <section id="outro-page">
-            <h1><span class="title-four">YOU</span>WIN</h1>
-            <button>Play Again</button>
-            </section>
-            `);
-    
-            var restartButton = document.querySelector('button');
-            restartButton.addEventListener ('click',createGameScreen);
-            }
-        
-    };
-    createWelcomeScreen();
-
-
-  
-    
-
-
+			var restartButton = document.querySelector('button');
+			restartButton.addEventListener ('click',createGameScreen);
+		}     
+	};
+		
+	createWelcomeScreen();
+	
 };
 
 window.addEventListener('load', main);
